664aea526f06ad62b0dd1956d954419591e78cd4
christianmbonet <christianm.bonet@gmail.com>
2021-10-04 10:53:41 -0400

    set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>r</div>
+      <div>e</div>
+      <div>i</div>
+      <div>g</div>
+      <div>n</div>
+      <div>b</div>
+      <div>o</div>
+    </div>
+  );
+}
+
+export default App;
